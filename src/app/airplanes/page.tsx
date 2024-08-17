8e2e6b806ada1c549ac8ebcc33cf5f2738e56ada
AmanRaza78 <razakhanaman9@gmail.com>
2024-08-17 23:11:39 +0530

    added the search bar

--- src/app/airplanes/page.tsx
+++ src/app/airplanes/page.tsx
@@ -1,15 +1,24 @@
 import AirPlaneCard from "@/components/airplane-card";
 import FilterSheet from "@/components/filter-sheet";
 import Pagination from "@/components/pagination";
+import SearchBar from "@/components/search-bar";
 import prisma from "@/lib/db";
 
 async function getData(searchParams: Record<string, string>) {
-  const { page, engineType, planeModal, capacity } = searchParams;
+  const { page, engineType, planeModal, capacity, query } = searchParams;
 
   const filters: any = {};
   if (engineType) filters.engineType = engineType;
   if (planeModal) filters.planeModal = planeModal;
   if (capacity) filters.capacity = capacity;
+
+  if (query) {
+    const lowerQuery = query.toLowerCase();
+    filters.OR = [
+      { planeName: { contains: lowerQuery} },
+      { planeModal: { contains: lowerQuery } },
+    ];
+  }
 
   const [count, data] = await prisma.$transaction([
     prisma.plane.count({
@@ -47,6 +56,10 @@ export default async function AirPlanesPage({
     <div className="flex flex-col">
       <div className="mt-4 mx-10">
         <FilterSheet />
+      </div>
+
+      <div className="mt-4 mx-10 flex">
+        <SearchBar />
       </div>
       <div className="py-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 md:gap-8 lg:gap-10 mx-10">
         {data.map((item) => (
